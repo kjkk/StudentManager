e594d854b7c78ce918073b2ff7fc218990de5233
khyatijha777 <khyatijha777@gmail.com>
2019-04-08 13:05:48 +0530

    dashbpoard updated

--- class_info/urls.py
+++ class_info/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('time_table', views.time_table)
+    path('time_table/', views.time_table),
+    path('classes_list/', views.classes_list)
 ]
--- class_info/views.py
+++ class_info/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def time_table(req):
     return render(req, 'class_info/time_table.html')
+
+def classes_list(req):
+    return render(req, 'class_info/classes_list.html')
